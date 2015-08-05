17117aad79a1dc0efc48f1fc0f059ac14b2fa070
banada <banada@localhost.localdomain>
2015-08-05 14:15:50 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # node-red-contrib-bluetooth-serial
-A Node-RED node to send and receive messages over Bluetooth Classic serial.
+A Node-RED node to send and receive messages over Bluetooth Classic serial. This is a useful way to make Node-RED talk to an Arduino wirelessly.
+
+##Usage
+
+```
+npm install node-red-contrib-bluetooth-serial
+node-red
+```
+
+![alt text](https://github.com/banada/node-red-contrib-bluetooth-serial/doc/screenshot1.png)
+
+Input a MAC address with the topic 'address' to start connecting
+
+![alt text](https://github.com/banada/node-red-contrib-bluetooth-serial/doc/screenshot2.png)
+
+##Dependencies
+
+bluez 5.0+
+
+bluetooth-serial-port (npm module)
+
+##Tested with:
+
+ORICO BTA-403 Bluetooth 4.0 USB Dongle
+
+HC-06 Bluetooth Module
+
+Fedora 21
+
+##License
+
+MIT License
+
+See doc/LICENSE
---
+++ bluetooth-serial.js
@@ -0,0 +1,56 @@
+module.exports = function(RED) {
+    var btSerial = new (require('bluetooth-serial-port')).BluetoothSerialPort();
+    function WriteBTSerial(config) {
+        RED.nodes.createNode(this, config);
+        this.btaddress = config.btaddress;
+        this.btname = config.btname;
+        var node = this;
+
+        this.on('input', function(msg) {
+            if (msg.topic === "address") {
+                var address = msg.payload;
+                console.log("connecting to: " + address);
+                msg.payload = "connecting to: " + address;
+                node.send(msg);
+                btSerial.findSerialPortChannel(address, function(channel) {
+                    btSerial.connect(address, channel, function() {
+                        console.log('connected to: ' + address);
+                        msg.payload = "Success! Connected to: " + address;
+                        var bufferhack = "";
+                        btSerial.on('data', function(buffer) {
+                            console.log(buffer);
+                            for (var i=0; i<buffer.length; i++) {
+                                if (buffer[i] != 10) {
+                                    var buf = new Buffer(1);
+                                    buf[0] = buffer[i];
+                                    bufferhack += buf.toString('ascii');
+                                } else {
+                                    msg.payload = bufferhack;
+                                    node.send(msg);
+                                    console.log(bufferhack);
+                                    bufferhack = "";
+                                    break;
+                                }
+                            }
+                        }); 
+                    });
+                });
+            } else {
+                var buffer = new Buffer(msg.payload);
+                msg.payload = buffer.toString('ascii');
+                node.send(msg);
+                btSerial.write(buffer, function(error, bytes) {
+                    if (error) {
+                        console.log("Bluetooth serial write error: " + error);
+                    } else {
+                        console.log("Wrote " + bytes + " bytes to Bluetooth serial");
+                    }
+                });
+            }
+        });
+
+        this.on('close', function() {
+        });
+    }
+    RED.nodes.registerType("bt-serial-write", WriteBTSerial);
+}
